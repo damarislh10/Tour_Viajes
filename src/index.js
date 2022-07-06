dccea331e42d0e6265638307338832f099a47e3f
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-07-06 12:54:54 -0500

    Styles artcile and web

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from "react-redux";
 import { TourApp } from './TourApp';
 import { store } from './redux/store/store';
