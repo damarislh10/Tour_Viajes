4d9aef44996fc7ccf7fcb70f7d7e50be81c8a4d3
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-07-06 14:56:24 -0500

    Styles updated

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
